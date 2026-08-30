4e8ba5017858881b3973efa60584a81bfa05a795
Martin Darius Alba <albamartindarius@gmail.com>
2026-08-30 22:53:54 +0800

    fix: added main url as backend root rather than local host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	server: {
 		proxy: {
-			'/api': 'http://127.0.0.1:8000', // change if needed
+			'/api': 'https://exosight-xs-ight.vercel.app/', // change if needed
 		},
 	},
 	plugins: [
