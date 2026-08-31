d7686252349242ae39d68f71282ce71c56e9f2e8
chraltsha <justinkennethco@gmail.com>
2026-09-01 00:06:22 +0800

    npm install leaftlet

--- frontend/.svelte-kit/generated/server/internal.js
+++ frontend/.svelte-kit/generated/server/internal.js
@@ -26,7 +26,7 @@ export const options = {
 		app: ({ head, body, assets, nonce, env }) => "<!doctype html>\r\n<html lang=\"en\">\r\n\t<head>\r\n\t\t<meta charset=\"utf-8\" />\r\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\r\n\t\t<meta name=\"text-scale\" content=\"scale\" />\r\n\t\t" + head + "\r\n\t</head>\r\n\t<body data-sveltekit-preload-data=\"hover\">\r\n\t\t<div style=\"display: contents\">" + body + "</div>\r\n\t</body>\r\n</html>\r\n",
 		error
 	},
-	version_hash: "13z46nl"
+	version_hash: "1pxihzq"
 };
 
 export async function get_hooks() {
