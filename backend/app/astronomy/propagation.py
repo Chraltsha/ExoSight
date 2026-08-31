300e3e467a7439eb0b238fba2bd87ce743c01e24
chraltsha <justinkennethco@gmail.com>
2026-09-01 00:43:47 +0800

    logs

--- backend/app/astronomy/propagation.py
+++ backend/app/astronomy/propagation.py
@@ -101,7 +101,7 @@ def propagate_satellite(
 
 def propagate_satellites(satellites, observer, observation_time, exposure_duration):
     from datetime import timedelta
-    print("starting propogation")
+    print("---------starting propogation-----")
     # Build all sample times as a Skyfield time array — one call
     n_samples = int(exposure_duration//10) + 1
     sample_times = [
